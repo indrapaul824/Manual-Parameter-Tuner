8374df7af4db81d1619e62ef2a055bb2936e41e2
INDRASHIS PAUL <indrashis985@gmail.com>
2021-01-06 13:31:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"indrashis985@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
-port = $PORT\n\
 enableCORS = false\n\
-\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
